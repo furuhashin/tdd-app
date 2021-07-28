dde0ada32c9b0aee95743a9616a596b633b983a0
nob <yasai.saita@gmail.com>
2021-07-28 19:48:02 +0900

    [add] Chapter2 P15 Dollarの副作用どうする？

--- app/Chapter1/Money/Dollar.php
+++ app/Chapter1/Money/Dollar.php
@@ -16,9 +16,10 @@ class Dollar
 
     /**
      * @param int $multiplier
+     * @return Dollar
      */
     public function times(int $multiplier)
     {
-        $this->amount *= $multiplier;
+        return new Dollar($this->amount * $multiplier);
     }
 }
--- tests/Unit/MoneyTest.php
+++ tests/Unit/MoneyTest.php
@@ -14,7 +14,10 @@ class MoneyTest extends TestCase
     public function Multiplication()
     {
         $five = new Dollar(5);
-        $five->times(2);
-        $this->assertEquals(10,$five->amount);
+        /** @var Dollar $product */
+        $product = $five->times(2);
+        $this->assertEquals(10,$product->amount);
+        $product = $five->times(3);
+        $this->assertEquals(15,$product->amount);
     }
 }
